f7d6ee4c5d419c75162f88152c300d40e4719bf1
DOCTOR_RABB <doctor.rabb@mail.ru>
2016-05-13 23:05:53 +0400

    Update gt.py

--- lib/graph/gt.py
+++ lib/graph/gt.py
@@ -5,18 +5,7 @@
 import pygtk
 pygtk.require ('2.0')
 import gtk
-
-
-class Events:
-    class WindowEvents:
-        @classmethod
-        def destr(self, widget, data=None):
-            gtk.main_quit()
-
-    class ButtonEvents:
-        @classmethod
-        def exit_button_func(self, widget):
-            raise SystemExit(0)
+from lib.graph import eventsys
 
 
 class MainWindow:
