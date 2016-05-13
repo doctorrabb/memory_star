77766546eaaa3203ce9a57f6542e080a2d592eb0
DOCTOR_RABB <doctor.rabb@mail.ru>
2016-05-13 23:08:32 +0400

    Update eventsys.py

--- lib/graph/eventsys.py
+++ lib/graph/eventsys.py
@@ -2,13 +2,13 @@ import pygtk
 pygtk.require ('2.0')
 import gtk
 
-class Events:
-    class WindowEvents:
+class Events (object):
+    class WindowEvents (object):
         @classmethod
         def destr(self, widget, data=None):
             gtk.main_quit()
 
-    class ButtonEvents:
+    class ButtonEvents (object):
         @classmethod
         def exit_button_func(self, widget):
             raise SystemExit(0)
