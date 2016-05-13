b49aacfd1f2b03f7722d7424d565dbf1d5b98eae
DOCTOR_RABB <doctor.rabb@mail.ru>
2016-05-14 03:34:58 +0400

    Update data.py

--- lib/memwork/data.py
+++ lib/memwork/data.py
@@ -1,11 +1,18 @@
+'''
+   This is a main data file. Here we have information about size of types and "binary start".
+   © DOCTOR_RABB
+'''
+
 import struct
 import sys
 
 BIT_ENDOF = ''
 
+# Let's detect user's byteorder.
 if sys.byteorder == 'little': BIT_ENDOF = '<'
 else: BIT_ENDOF = '>'
 
+# Class with types size.
 class SizeInfo (object):
     boolean = struct.calcsize ('b')
     integer = struct.calcsize ('i')
